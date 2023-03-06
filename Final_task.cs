16ffff0b4229ac93645ea4d317286ad0415a6cf8
VecheslavSP <willy0@rambler.ru>
2023-03-06 18:08:12 +0300

    решена задача

--- Final_task.cs
+++ Final_task.cs
@@ -19,7 +19,7 @@ class Program
     static String Build_element()
     {
         Random rand = new Random();
-        int len = rand.Next(1, 5);
+        int len = rand.Next(1, 20);
         char[] a = new char[len];
         for (int i = 0; i < len; i++)
         {
@@ -28,11 +28,34 @@ class Program
         string str = new string(a);
         return str;
     }
+     static String[] FinalyStringArray(String[] StartArray)
+    {
+        byte len =Convert.ToByte(StartArray.Length);
+        String[] FinalyArray=new String[1];
+        for (byte i = 0; i < len; i++)
+        {
+            if(StartArray[i].Length<4){
+                 Array.Resize(ref FinalyArray, FinalyArray.Length + 1);
+                FinalyArray[FinalyArray.Length-1]=StartArray[i];
+                
+            }
+        }
+if(FinalyArray.Length==1){
+    FinalyArray[0]=" нет подходящих под условие строк";
+}
+        return FinalyArray;
+    }
     public static void Main()
     {
         string[] ArrayString = BuildStartStringArray();
         Console.WriteLine("Исходный массив");
         foreach (string i in ArrayString)
+        {
+            Console.WriteLine(i);
+        }
+        string[] FinalyArray=FinalyStringArray(ArrayString);
+         Console.WriteLine("Выходной массив");
+         foreach (string i in FinalyArray)
         {
             Console.WriteLine(i);
         }
